d7e67861cd0d9cf8a7e7d1cae16d59e7b51674c9
Mark Eliasen <mark.eliasen@pm.me>
2024-12-06 13:05:58 +0000

    feat: add read cmd

---
+++ internal/cmd/read.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mreliasen/scrolls-cli/internal/scrolls"
+	"github.com/spf13/cobra"
+)
+
+var readCmd = &cobra.Command{
+	Use:   "read <scroll-name>",
+	Short: "writes the scroll's content to stdout",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+
+		c, err := scrolls.New()
+		if err != nil {
+			fmt.Printf("failed to initiate scroll-cli")
+			return
+		}
+
+		s := c.Files.GetScroll(name)
+		if s.Type == "plain-text" {
+			fmt.Printf("unable to cast a plain text scroll")
+			return
+		}
+
+		os.Stdout.Write([]byte(s.Body()))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(readCmd)
+}
